625add994c818d52d064b34446e0dc9a574ebee2
Eduard Funnekotter <efunneko@gmail.com>
2018-05-10 21:01:29 -0400

    remove creds

--- common/env.js
+++ common/env.js
@@ -1,15 +1,10 @@
 export default {
   title: "Solace Symphony",
   broker: {
-    url: "mqtts://mrukoj6dbd.messaging.solace.cloud:1883",
-    smfurl: "ws://mrukoj6dbd.messaging.solace.cloud:80",
-    smfvpn: "msgvpn-ukoj6d9z",
-    username: "solace-cloud-client",
-    password: "vgvfl6rd7d7laa9p50vb4nsvt8"
-    //url: "mqtts://192.168.169.90:1883",
-    //smfurl: "ws://192.168.169.90:80",
-    //smfvpn: "default",
-    //username: "default",
-    //password: "mydemopassword"
+    url: "",
+    smfurl: "",
+    smfvpn: "",
+    username: "",
+    password: ""
   }
 };
